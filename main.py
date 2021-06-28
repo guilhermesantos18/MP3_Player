1285e54b80ee67595b06d1649a25d4f5a552a8f1
Guilherme Santos <guipinela.santos@gmail.com>
2021-06-28 20:07:58 +0100

    Commit

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 # Biblioteca GUI
 import tkinter as tk
-from tkinter import *
+# Reproduzir a música
+from pygame import mixer
+import os
+
+
+# Abrir explorador de arquivos para escolher a pasta
+def abrir_explorador_de_arquivos():
+    os.popen('explorer')
 
 
 # Interface
@@ -13,14 +20,18 @@ menu = tk.Menu(janela_principal)
 janela_principal.config(menu=menu)
 music_menu = tk.Menu(menu)
 menu.add_cascade(label='Opções', menu=music_menu)
-music_menu.add_command(label='Pasta Musica')
+music_menu.add_command(label='Pasta Musica', command=abrir_explorador_de_arquivos)
 music_menu.add_command(label='Musica especifíca')
 
 # Icones
-icone_stop = tk.PhotoImage(file='C:\Programação e Projetos\Linguagens que mais uso e trabalho\Python\projetospython\MP3_Player\Icons\outline_pause_black_24dp.png')
-icone_play = tk.PhotoImage(file='C:\Programação e Projetos\Linguagens que mais uso e trabalho\Python\projetospython\MP3_Player\Icons\outline_play_arrow_black_24dp.png')
-icone_next = tk.PhotoImage(file='C:\Programação e Projetos\Linguagens que mais uso e trabalho\Python\projetospython\MP3_Player\Icons\outline_skip_next_black_24dp.png')
-icone_previous = tk.PhotoImage(file='C:\Programação e Projetos\Linguagens que mais uso e trabalho\Python\projetospython\MP3_Player\Icons\outline_skip_previous_black_24dp.png')
+icone_stop = tk.PhotoImage(
+    file='C:\Programação e Projetos\Linguagens que mais uso e trabalho\Python\projetospython\MP3_Player\Icons\outline_pause_black_24dp.png')
+icone_play = tk.PhotoImage(
+    file='C:\Programação e Projetos\Linguagens que mais uso e trabalho\Python\projetospython\MP3_Player\Icons\outline_play_arrow_black_24dp.png')
+icone_next = tk.PhotoImage(
+    file='C:\Programação e Projetos\Linguagens que mais uso e trabalho\Python\projetospython\MP3_Player\Icons\outline_skip_next_black_24dp.png')
+icone_previous = tk.PhotoImage(
+    file='C:\Programação e Projetos\Linguagens que mais uso e trabalho\Python\projetospython\MP3_Player\Icons\outline_skip_previous_black_24dp.png')
 
 # Criar a Label para icones
 stop_music = tk.Label(janela_principal, image=icone_stop)
